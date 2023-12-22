74b1836d93da15ab9617304c6aa0ec5413c163d0
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-12-22 09:22:55 -0700

    skeleton project cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,33 @@
-import { ReactElement } from "react";
-
 export default function Index() {
   return (
-    <div className="flex flex-col lg:flex-row gap-4">
-      <div className="flex flex-col gap-4">
-        <div className="bg-slate-800 p-4 rounded-xl">
-          Take a look at my projects {"->"}
-        </div>
-      </div>
-      <div className="bg-slate-800 p-4 rounded-xl max-w-80">
+    <div className="flex flex-col gap-4 max-w-prose">
+      <div className="bg-slate-800 p-4 rounded-xl">
         <h2 className="text-2xl text-amber-200 font-semibold">Profile</h2>
         <p>
-          Joshua Kellogg is a full-stack web developer from Denver, Colorado.
+          My name is Joshua Kellogg, and I'm a full-stack web developer from
+          Denver, Colorado.
+        </p>
+        <p>
+          In the fall of 2023 I completed a 12-week bootcamp that taught me a
+          wide variety of web development skills. In The time since, I've been
+          working on a variety of projects. Most recently, I've been using{" "}
+          <a
+            href="https://nextjs.org/"
+            className="text-amber-100 underline"
+            target="_blank"
+          >
+            Next.js
+          </a>
+          to build my personal site (the one you're looking at now), as well as
+          working through{" "}
+          <a
+            href="https://craftinginterpreters.com/"
+            className="text-amber-100 underline"
+            target="_blank"
+          >
+            Bob Nystrom's Crafting Interpreters
+          </a>
+          , which has been a really fun and interesting experience.
         </p>
       </div>
     </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,30 @@
+import ProjectCard from "@/components/ProjectCard";
+
 export default function Projects() {
-  return "hello from projects page";
+  return (
+    <div className="grid grid-cols-2 max-w-prose gap-4">
+      <ProjectCard
+        title="Example Project"
+        links={[
+          {
+            name: "Repo",
+            url: "#",
+          },
+        ]}
+      >
+        This is an example project card
+      </ProjectCard>
+      <ProjectCard
+        title="Example Project"
+        links={[
+          {
+            name: "Repo",
+            url: "#",
+          },
+        ]}
+      >
+        This is an example project card
+      </ProjectCard>
+    </div>
+  );
 }
