b07bc89a3cc054bbb373766117edc733dd59bfde
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-12-22 21:58:06 -0700

    responsive design for nameplate

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,17 +17,19 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body className="bg-slate-950 text-amber-50">
-        <div className="flex flex-row justify-center items-start p-2 lg:p-8 gap-4">
-          <div className="flex flex-col gap-4">
+        <div className="flex flex-col sm:flex-row justify-center items-start p-2 lg:p-8 gap-4">
+          <div className="flex flex-col gap-4 max-sm:w-full">
             <Link
-              className="bg-slate-800 p-4 rounded-xl text-end hover:bg-slate-700 transition-colors duration-200"
+              className="bg-slate-800 p-4 rounded-xl sm:text-end hover:bg-slate-700 transition-colors duration-200"
               href="/"
             >
-              <h1 className="text-amber-200 font-sans text-3xl font-bold">
-                Joshua Kellogg
+              <h1 className="text-amber-200 sm:text-3xl font-bold block">
+                Joshua
+                <br className="max-sm:hidden lg:hidden" /> Kellogg
               </h1>
-              <h3 className="text-amber-100 text-xl font-semibold">
-                Full-Stack Developer
+              <h3 className="text-amber-100 sm:text-xl font-semibold block">
+                Full-Stack
+                <br className="max-sm:hidden lg:hidden" /> Developer
               </h3>
             </Link>
             <Nav />
