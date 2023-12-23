784f5f276194d3c167a0513c3869392801ddad08
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-12-22 21:29:14 -0700

    active state for nav links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type NavItem = {
   name: string;
@@ -6,10 +9,11 @@ type NavItem = {
 };
 
 export default function Nav() {
+  const pathname = usePathname();
+
   const navItems: NavItem[] = [
     {
       name: "Projects",
-      url: "/projects",
     },
     {
       name: "Essays",
@@ -21,15 +25,25 @@ export default function Nav() {
 
   return (
     <nav className="bg-slate-800 p-2 rounded-xl text-end text-lg">
-      {navItems.map(({ name, url }, index) => (
-        <Link
-          key={index}
-          className="block p-2 hover:bg-slate-700 hover:text-amber-200 rounded-lg transition-colors duration-200"
-          href={url ? url : "/"}
-        >
-          {name}
-        </Link>
-      ))}
+      {navItems.map(({ name, url }, index) => {
+        return pathname === url ? (
+          <Link
+            key={index}
+            className="block p-2 bg-slate-900 text-amber-300 underline rounded-lg cursor-default"
+            href={url}
+          >
+            {name}
+          </Link>
+        ) : (
+          <Link
+            key={index}
+            className="block p-2 hover:bg-slate-700 hover:text-amber-200 rounded-lg transition-colors duration-200"
+            href={url ? url : "/not-found"}
+          >
+            {name}
+          </Link>
+        );
+      })}
     </nav>
   );
 }
