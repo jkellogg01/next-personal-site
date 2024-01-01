49ae7710f77a7d76b83a5188e33f52988be4c8f5
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-01-01 14:49:49 -0700

    contact form enforces validation

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -7,18 +7,26 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req: Request) {
 	try {
 		const data = JSON.parse(await req.text());
+		console.log("recieved data from contact form");
+		console.log(data);
 
 		const opts: CreateEmailOptions = {
-			from: "<contact@jkellogg.dev>",
+			from: "contact@jkellogg.dev",
 			to: ["kellogg.20.joshua@gmail.com"],
 			subject: "New Contact Form Submitted",
 			react: EmailTemplate(data),
 		};
 
-		const emailResp = await resend.emails.send(opts);
+		const { data: resp, error } = await resend.emails.send(opts);
 
-		return Response.json(emailResp);
-	} catch (err) {
-		return Response.json({ err });
+		if (error) {
+            console.error(error);
+			return Response.json(error, { status: 500 });
+		}
+        console.log(resp)
+		return Response.json(resp, { status: 200 });
+	} catch (error) {
+        console.error(error)
+		return Response.json(error, { status: 500 });
 	}
 }
