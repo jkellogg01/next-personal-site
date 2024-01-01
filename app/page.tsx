352977122bb52084dffa7f67996798142dc58f04
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-01-01 14:58:10 -0700

    removed projects page from nav. needs copy before i can comfortably call that ready to go

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default function Index() {
   const navCards: NavCard[] = [
     {
       name: "Projects",
-      url: "/projects",
       desc: "Some of the projects I've worked on recently",
     },
     {
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -15,7 +15,6 @@ export default function Nav() {
   const navItems: NavItem[] = [
     {
       name: "Projects",
-      url: "/projects",
     },
     {
       name: "Essays",
